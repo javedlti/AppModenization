72bf312983813562a827fda00801e78e6732ba09
javedlti <javed.ahmad@exxonmobil.com>
2022-10-24 18:57:59 +0530

    Creating Console App to Web Application

---
+++ ConsoleToWebCore/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace ConsoleToWebCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateBuilder(string[] args)
+        {
+           return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(web =>
+            {
+                web.UseStartup<Startup>();
+            });
+        }
+    }
+}
